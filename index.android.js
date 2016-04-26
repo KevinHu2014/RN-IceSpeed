07461d862367ad5f28c6698ec1ea6dac24480eea
胡翔喻 <Kevin@huxiangyude-MacBook-Air.local>
2016-04-26 20:43:01 +0800

    add battle

--- index.android.js
+++ index.android.js
@@ -1,4 +1,5 @@
 var React = require('react-native');
 var {AppRegistry} = React;
-var test = require('./tab');
+//var test = require('./tab');
+var test = require('./battle');
 AppRegistry.registerComponent('test', () => test);
