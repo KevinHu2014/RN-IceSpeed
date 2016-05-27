4018539659b01b1b2fb1b11310af791331e386f9
胡翔喻 <402261094@mail.fju.edu.tw>
2016-05-27 09:49:14 +0800

    small change to status bar

--- MapExample.js
+++ MapExample.js
@@ -238,7 +238,16 @@ var MapExample = React.createClass({
        
        
         <View style={styles.status}>
-          <Statusbar></Statusbar>
+          <View style={styles.Top_status}>
+            <View style={styles.Top_status_L}>
+              <Text style={styles.Top_welcome_L}>火</Text>
+            </View>
+            <View style={styles.Top_status_R}>
+                <TouchableOpacity onPress={this._pressButton}>
+                  <Text style={styles.Top_welcome_R}>冰櫃神速</Text>
+                </TouchableOpacity>
+            </View>
+          </View>
         </View>
         <Collapsible collapsed={this.state.collapsed} align="center">
           <View style={styles.explore}>
@@ -326,6 +335,41 @@ var styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: 'column',
+  },
+  Top_status: {
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: '#8E8E8E',
+  },
+  Top_status_L: {
+    flex: 1,
+    backgroundColor: '#EA0000',
+    borderWidth: 5,
+    borderColor: '#FFD306',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 100,
+    borderTopRightRadius: 0,
+
+  },
+  Top_welcome_L: {
+    fontSize: 30,
+    color: '#FFFFFF',
+  },
+  Top_status_R:{
+    flex: 4,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#d0d0d0',
+    borderWidth: 10,
+    borderColor: '#5B5B5B',
+    borderTopColor: '#A0A0A0',
+    borderLeftColor: '#8E8E8E',
+    borderRightColor: '#858585',
+  },
+  Top_welcome_R: {
+    fontSize: 30,
+    color: 'black',
   },
   status: {
     flex: 1,
