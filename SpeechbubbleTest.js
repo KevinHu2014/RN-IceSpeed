cc250ac55c3ab796503122c25c35665e863b84de
胡翔喻 <402261094@mail.fju.edu.tw>
2016-06-27 00:43:25 +0800

    add speech bubble test

---
+++ SpeechbubbleTest.js
@@ -0,0 +1,28 @@
+
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableHighlight,
+} from 'react-native';
+import SpeechBubble from 'react-native-speech-bubble';
+
+var SpeechbubbleTest = React.createClass({
+  render() {
+    return (
+      <View> 
+        <View style={{flex: 6,}}></View>
+        <View style={{flex: 1,}}>
+          <SpeechBubble allowSpeechReplay={true}
+            speeches={[ 'String 1', 'String 2', 'String 3' ]}
+          />
+        </View>
+      </View>
+    );
+  }
+});
+var styles = StyleSheet.create({
+
+ });
+module.exports = SpeechbubbleTest;
--- index.android.js
+++ index.android.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import {AppRegistry} from 'react-native';
-var test = require('./tab');
+//var test = require('./tab');
 //var test = require('./Pet');
 //var test = require('./battle');
 //var test = require('./SampleComponent');
@@ -9,4 +9,5 @@ var test = require('./tab');
 //var test = require('./SensorTest');
 //var test = require('./NavPet');
 //var test = require('./Box');
+var test = require('./SpeechbubbleTest');
 AppRegistry.registerComponent('test', () => test);
