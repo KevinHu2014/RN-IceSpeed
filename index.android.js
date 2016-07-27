e9500d34cd92441c92302a702fe776cae897d9eb
胡翔喻 <402261094@mail.fju.edu.tw>
2016-07-27 20:58:49 +0800

    upgrade mapbox to 5.0.0

--- index.android.js
+++ index.android.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import {AppRegistry} from 'react-native';
-var test = require('./tab');
+//var test = require('./tab');
 //var test = require('./battle');
 //var test = require('./SampleComponent');
 //var test = require('./NavBox');
@@ -11,4 +11,5 @@ var test = require('./tab');
 //var test = require('./NavPet');
 //var test = require('./SpeechbubbleTest');
 //var test = require('./FetchTest');
+var test = require('./NewMap');
 AppRegistry.registerComponent('test', () => test);
