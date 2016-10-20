dea592bd2951b215a4209be5e7391e2d68686ca9
胡翔喻 <402261094@mail.fju.edu.tw>
2016-10-21 04:04:17 +0800

    Link gym_battle to Battle_LandingPage

--- gym_battle.js
+++ gym_battle.js
@@ -32,7 +32,7 @@ import {
 } from 'react-native';
 
 var {height, width} = Dimensions.get('window');
-
+import Battle_LandingPage from './Battle_LandingPage';
 
 var gym_battle = React.createClass({
   getInitialState(){
@@ -44,6 +44,7 @@ var gym_battle = React.createClass({
       gymWelcome: '勇敢的冒險者阿！讓我看看你經過磨練後綻放的光芒吧！',
       Monster_name: '小煤礦',
       Monster_Img: 'https://s19.postimg.org/ok4g3gjxf/010_r.png',
+      Monster_id: null,
       Stone_Img : 'https://s10.postimg.org/3nf56l03t/Attributes_Leaf.png',
       BigThree_Img : 'https://s19.postimg.org/jht4w2rc3/waffle.png',
       BigThree : '鬆餅', 
@@ -62,6 +63,7 @@ var gym_battle = React.createClass({
           gymLeader: '克雷斯',
           gymWelcome: '勇敢的冒險者阿！讓我看看你經過磨練後綻放的光芒吧！',
           gymAvatar: 'https://s19.postimg.org/hor0bw2jn/avatar2.png',
+          Monster_id: 10,
           Monster_name: '小煤礦',
           Monster_Img: 'https://s19.postimg.org/ok4g3gjxf/010_r.png', 
         });
@@ -71,6 +73,7 @@ var gym_battle = React.createClass({
           gymLeader: '莉莉亞',
           gymWelcome: '來吧~! 太小看莉莉亞的話可是會吃大虧的哦~ 全力以赴的決鬥吧！',
           gymAvatar: 'https://s19.postimg.org/ryyh88yqr/avatar1.png',
+          Monster_id: 12,
           Monster_name: '爆焰精靈',
           Monster_Img: 'https://s19.postimg.org/q1pwf0oo3/012_r.png',  
         });
@@ -80,6 +83,7 @@ var gym_battle = React.createClass({
           gymLeader: '約瑟夫',
           gymWelcome: '呵~呵呵，年輕真好！總是充滿著熱情與想法，但是在這方面老夫也是不會輸給任何人的！',
           gymAvatar: 'https://s19.postimg.org/uvrivxl9f/2.png',
+          Monster_id: 9,
           Monster_name: '麻糬長老',
           Monster_Img: 'https://s19.postimg.org/gq3ui2c4j/009_r.png',   
         });
@@ -89,6 +93,7 @@ var gym_battle = React.createClass({
           gymLeader: '艾芙拉',
           gymWelcome: '叮叮噹~小心點！艾芙拉的寵物可是超~~~~級~~~~~強的哦！！！',
           gymAvatar: 'https://s19.postimg.org/tzqg2qm77/艾芙拉.png',
+          Monster_id: 11,
           Monster_name: '火焰精靈',
           Monster_Img: 'https://s19.postimg.org/jmqvichyb/011_r.png',   
         });
@@ -98,6 +103,7 @@ var gym_battle = React.createClass({
           gymLeader: '巴澤爾',
           gymWelcome: '知識就是力量，在全知的我面前，你毫無勝算。',
           gymAvatar: 'https://s19.postimg.org/vk5fbzqar/巴澤爾.png',
+          Monster_id: 3,
           Monster_name: '肥菇',
           Monster_Img: 'https://s19.postimg.org/5vht2e3mr/003_r.png',   
         });
@@ -107,6 +113,7 @@ var gym_battle = React.createClass({
           gymLeader: '漢娜',
           gymWelcome: '哎呀呀~來了個可愛的孩子呢~讓姊姊來好好疼愛你吧~！ <3',
           gymAvatar: 'https://s19.postimg.org/ylr5otx43/漢娜.png',
+          Monster_id: 2,
           Monster_name: '肥菇',
           Monster_Img: 'https://s19.postimg.org/ww9ufkd0j/002_r.png',   
         });
@@ -116,6 +123,7 @@ var gym_battle = React.createClass({
           gymLeader: '漢娜',
           gymWelcome: '哎呀呀~來了個可愛的孩子呢~讓姊姊來好好疼愛你吧~！ <3',
           gymAvatar: 'https://s19.postimg.org/ylr5otx43/漢娜.png',
+          Monster_id: 7,
           Monster_name: '葉子麻糬',
           Monster_Img: 'https://s19.postimg.org/h06rno1cz/007_r.png',   
         });
@@ -125,6 +133,7 @@ var gym_battle = React.createClass({
           gymLeader: '艾力克斯',
           gymWelcome: '貫徹你的信念，你將會獲得無可限量的力量，解放你赤誠的心吧！！！',
           gymAvatar: 'https://s19.postimg.org/6o04zuk83/avatar3.png',
+          Monster_id: 6,
           Monster_name: '獨眼怪',
           Monster_Img: 'https://s19.postimg.org/7r4ldjsgz/006_r.png',   
         });
@@ -134,6 +143,7 @@ var gym_battle = React.createClass({
           gymLeader: '狄倫',
           gymWelcome: '在這世界中，力量就是一切，弱小的人是無法守護自己最珍視的東西的',
           gymAvatar: 'https://s19.postimg.org/ygrzswf6r/狄倫.png',
+          Monster_id: 14,
           Monster_name: '烏賊船長',
           Monster_Img: 'https://s19.postimg.org/xk91nng0z/014_r.png',   
         });
@@ -143,6 +153,7 @@ var gym_battle = React.createClass({
           gymLeader: '卡洛琳',
           gymWelcome: '嗨嗨~精靈怪物真的是很神奇的生物呢~想要來比試一下嗎？',
           gymAvatar: 'https://s19.postimg.org/jkcgogp2b/卡洛琳.png',
+          Monster_id: 17,
           Monster_name: '胡頭神狼',
           Monster_Img: 'https://s19.postimg.org/qu27p2okj/017_r.png',   
         });
@@ -152,6 +163,7 @@ var gym_battle = React.createClass({
           gymLeader: '艾爾文',
           gymWelcome: '靜下心來，你將會感受到自然之力的湧現，追求心境上的平靜之人，願愛與勇氣與你同行',
           gymAvatar: 'https://s19.postimg.org/otqboohyb/艾爾文.png',
+          Monster_id: 16,
           Monster_name: '胡頭狼',
           Monster_Img: 'https://s19.postimg.org/jpkeg1hb7/016_r.png',   
         });
@@ -161,6 +173,7 @@ var gym_battle = React.createClass({
           gymLeader: '艾咪',
           gymWelcome: '嗚咪~我細心照顧的寵物可是很強的哦！！！看招吧！',
           gymAvatar: 'https://s19.postimg.org/eyz6ige03/艾咪.png',
+          Monster_id: 5,
           Monster_name: '小魔眼',
           Monster_Img: 'https://s19.postimg.org/de0yb0uzn/005_r.png',   
         });
@@ -170,6 +183,7 @@ var gym_battle = React.createClass({
           gymLeader: '安琪拉',
           gymWelcome: '看來您已經對遊戲有了基本的認識了呢~那接下來就由在下來做您的對手吧！',
           gymAvatar: 'https://s19.postimg.org/wv6pdoo9v/1.png',
+          Monster_id: 15,
           Monster_name: '玉米老大',
           Monster_Img: 'https://s19.postimg.org/3tlx1vv1f/015_r.png',   
         });
@@ -210,6 +224,36 @@ var gym_battle = React.createClass({
                 //返回MultiMap
                 navigator.pop();
             }
+  },
+  Battle(Lv: number){
+    const { navigator } = this.props;
+        if(navigator) {
+            navigator.push({
+                name: 'Battle_LandingPage',
+                component: Battle_LandingPage,
+                params: {
+                    Monster_Img: this.state.Monster_Img,
+                    Monster_name: this.state.Monster_name,
+                    Monster_Lv: Lv,
+                    Monster_id: this.state.Monster_id,
+                    getResult:(result) => {
+                        this.setState({result: result})
+                    }
+                 }   
+            })
+        }
+  },
+  onPress_select_1(){
+    this.Battle(10);
+  },
+  onPress_select_2(){
+    this.Battle(20);
+  },
+  onPress_select_3(){
+    this.Battle(30);
+  },
+  onPress_select_4(){
+    this.Battle(40);
   },
   render: function() {
     return (
@@ -293,7 +337,7 @@ var gym_battle = React.createClass({
             <View style={styles.Content}>
               <TouchableHighlight
                 style={styles.touchable}
-                onPress={this.onPress_select}>
+                onPress={this.onPress_select_1}>
                   <View style={[styles.button_color,{backgroundColor: '#D1E9EA'}]}>
                       <Text style={styles.button_welcome}>
                         {'初級 \b\b\b\bx 0'}
@@ -302,7 +346,7 @@ var gym_battle = React.createClass({
               </TouchableHighlight>
               <TouchableHighlight
                 style={styles.touchable}
-                onPress={this.onPress_select}>
+                onPress={this.onPress_select_2}>
                   <View style={[styles.button_color,{backgroundColor:'#415F77'}]}>
                       <Text style={[styles.button_welcome,{color: '#FFFFFF'}]}>
                         {'中級 \b\bx 50'}
@@ -311,7 +355,7 @@ var gym_battle = React.createClass({
               </TouchableHighlight>
               <TouchableHighlight
                 style={styles.touchable}
-                onPress={this.onPress_select}>
+                onPress={this.onPress_select_3}>
                   <View style={[styles.button_color,{backgroundColor:'#FFD00C'}]}>
                       <Text style={[styles.button_welcome,{color: '#FFFFFF'}]}>
                         {'上級\b\b x 70'}
@@ -320,7 +364,7 @@ var gym_battle = React.createClass({
               </TouchableHighlight>
               <TouchableHighlight
                 style={styles.touchable}
-                onPress={this.onPress_select}>
+                onPress={this.onPress_select_4}>
                   <View style={[styles.button_color,{backgroundColor:'#FC5050'}]}>
                       <Text style={[styles.button_welcome,{color: '#FFFFFF'}]}>
                         {'究級 x 100'}
