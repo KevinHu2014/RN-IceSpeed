cc42cf30da27b4b97db7c688e5cdddc71d61b3b9
胡翔喻 <402261094@mail.fju.edu.tw>
2016-10-25 02:19:44 +0800

    Update UI of ActionButton

--- testAction.js
+++ testAction.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import { StyleSheet, View } from 'react-native';
 import ActionButton from 'react-native-action-button';
-import Icon from 'react-native-vector-icons/Ionicons';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import Icon1 from 'react-native-vector-icons/Octicons';
 
 
 
@@ -12,14 +13,17 @@ var testAction = React.createClass({
       <View style={{flex:1, backgroundColor: '#f3f3f3'}}>
         {/*Rest of App come ABOVE the action button component!*/}
         <ActionButton buttonColor="rgba(231,76,60,1)">
-          <ActionButton.Item buttonColor='#9b59b6' title="New Task" onPress={() => console.log("notes tapped!")}>
-            <Icon name="md-create" style={styles.actionButtonIcon} />
+          <ActionButton.Item buttonColor='#9b59b6' title="設定" onPress={() => console.log("notes tapped!")}>
+            <Icon name="settings" style={styles.actionButtonIcon} />
           </ActionButton.Item>
-          <ActionButton.Item buttonColor='#3498db' title="Notifications" onPress={() => {}}>
-            <Icon name="md-notifications-off" style={styles.actionButtonIcon} />
+          <ActionButton.Item buttonColor='#3498db' title="圖鑑" onPress={() => {}}>
+            <Icon1 name="book" style={styles.actionButtonIcon} />
           </ActionButton.Item>
-          <ActionButton.Item buttonColor='#1abc9c' title="All Tasks" onPress={() => {}}>
-            <Icon name="md-done-all" style={styles.actionButtonIcon} />
+          <ActionButton.Item buttonColor='#1abc9c' title="道具" onPress={() => {}}>
+            <Icon1 name="beaker" style={styles.actionButtonIcon} />
+          </ActionButton.Item>
+          <ActionButton.Item buttonColor='#EFC02F' title="寵物" onPress={() => {}}>
+            <Icon1 name="squirrel" style={styles.actionButtonIcon} />
           </ActionButton.Item>
         </ActionButton>
       </View>
